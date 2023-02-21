a8b0cb141833dfc2dfc9c7d3ec7bf2060a9b181c
kumawat30rahul <kumawat30rahul@gmail.com>
2023-02-21 21:28:19 +0530

    sdhsdf

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your code here
+const gameContainer = document.getElementById('gameContainer')
+
+for (let i = 0; i < 1600; i++) {
+    let pixelDiv = document.createElement('div')
+    pixelDiv.id = `pixel${i}`
+    pixelDiv.classList.add('pixel_div')
+    gameContainer.appendChild(pixelDiv)
+}
