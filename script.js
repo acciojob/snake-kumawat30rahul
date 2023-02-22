6937b7f048e51828e698096ff6edac688e90a95b
kumawat30rahul <kumawat30rahul@gmail.com>
2023-02-22 16:16:28 +0530

    sdjbfsdf

--- script.js
+++ script.js
@@ -7,3 +7,40 @@ for (let i = 0; i < 1600; i++) {
     pixelDiv.classList.add('pixel_div')
     gameContainer.appendChild(pixelDiv)
 }
+let speed = 5;
+let lastupdateTime = 0;
+let snakePosition = [
+    {
+        x: 800,
+        y: 800
+    }
+]
+function main(ctime) {
+    window.requestAnimationFrame(main)
+    let elapsedTime = ctime -lastupdateTime   
+    if(elapsedTime >100){
+        gameEngine()
+        lastupdateTime = ctime
+    }
+}
+
+
+
+function gameEngine() {
+    // console.log(x++);
+    snakePosition[0].x += 1
+    snakePosition[0].y += 1
+
+    //==displaying the snake==//
+    snakePosition.forEach((e,index)=>{
+        let snakeHead = document.createElement("div")
+        snakeHead.style.gridRowStart = e.y
+        snakeHead.style.gridColumnStart = e.x
+        snakeHead.style.color ='yellow'
+        // gameContainer.appendChild(snakeHead)
+    })
+}
+
+// setInterval(function () {
+    window.requestAnimationFrame(main)
+// }, 100)
